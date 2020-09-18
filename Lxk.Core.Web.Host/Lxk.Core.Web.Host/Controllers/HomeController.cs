ae345d9c80fa7e160d9f9c9860b6192024c2f9fe
晓凯 李 <li1066784512@hotmail.com>
2020-09-18 17:58:43 +0800

    修改配置

---
+++ Lxk.Core.Web.Host/Lxk.Core.Web.Host/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Abp.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lxk.Core.Web.Host.Controllers
+{
+    public class HomeController : AbpController
+    {
+        public IActionResult Index()
+        {
+            return Redirect("/swagger");
+        }
+    }
+}
